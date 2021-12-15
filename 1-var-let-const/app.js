ee4b1bc47c1c62706fc50a53e9ae9bd508a21b4b
Christian Lisangola <jochri3@gmail.com>
2021-12-15 12:11:56 +0300

    var let const

---
+++ 1-var-let-const/app.js
@@ -0,0 +1,49 @@
+// 1 - Hoisiting
+// console.log("Pays : ", pays);
+
+// var pays = "Chine"; //pays=undefined
+
+// 2 - portée
+// for (var i = 0; i <= 5; i++) {
+//   console.log("Dans la boucle : ", i);
+// }
+// console.log("En dehors de la boucle : ", i);
+
+// if (true) {
+//   var ville = "Chicago";
+// }
+
+// console.log(ville);
+
+// function disBonjour() {
+//   var fruit = "Pomme";
+//   console.log("dans la fonction : ", fruit);
+// }
+
+// disBonjour();
+// console.log("En dehors de la fonction : ", fruit);
+// console.log(pays);
+
+// Variables
+let pays = "France";
+pays = "Italie";
+pays = "USA";
+
+console.log(pays);
+
+// constantes
+const ville = "Paris";
+// ville="Lyon" : erreur
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Dans la boucle : ", i);
+}
+// console.log("En dehors de la boucle : ", i);//erreur
+
+if (true) {
+  let annee = 2022;
+}
+console.log("Année : ", annee);
+
+
+const PI=3.14
